72333119ed06bd1ad67064b441e3e1444380c8df
tomalbrc <tomalbrc@me.com>
2024-04-14 20:09:19 +0200

    Force polymer to autohost resourcepack, v1.0.1

--- src/main/java/de/tomalbrc/danse/Danse.java
+++ src/main/java/de/tomalbrc/danse/Danse.java
@@ -6,12 +6,18 @@ import de.tomalbrc.danse.registries.PlayerModelRegistry;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Danse implements ModInitializer {
     public static final String MODID = "danse";
 
     @Override
     public void onInitialize() {
+        forceAutohost();
         PolymerResourcePackUtils.addModAssets(MODID);
         PolymerResourcePackUtils.markAsRequired();
 
@@ -19,5 +25,15 @@ public class Danse implements ModInitializer {
         EntityRegistry.registerMobs();
 
         CommandRegistrationCallback.EVENT.register((dispatcher, context, selection) -> GestureCommand.register(dispatcher));
+    }
+
+    void forceAutohost() {
+        try {
+            Path path = FabricLoader.getInstance().getGameDir().resolve("polymer/.force_autohost");
+            Files.createDirectories(path.getParent()); // Create parent directories if they don't exist
+            Files.createFile(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
