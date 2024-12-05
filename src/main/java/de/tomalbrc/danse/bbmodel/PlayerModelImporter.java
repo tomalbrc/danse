b675aed4534bc2ecb5ac6e6db855ee3e3e47d0e3
tomalbrc <tomalbrc@me.com>
2024-12-05 02:43:21 +0100

    1.21.4 update

--- src/main/java/de/tomalbrc/danse/bbmodel/PlayerModelImporter.java
+++ src/main/java/de/tomalbrc/danse/bbmodel/PlayerModelImporter.java
@@ -3,10 +3,14 @@ package de.tomalbrc.danse.bbmodel;
 import de.tomalbrc.bil.file.bbmodel.BbModel;
 import de.tomalbrc.bil.file.bbmodel.BbOutliner;
 import de.tomalbrc.bil.file.extra.BbResourcePackGenerator;
-import de.tomalbrc.bil.file.extra.ResourcePackItemModel;
+import de.tomalbrc.bil.file.extra.ResourcePackModel;
 import de.tomalbrc.bil.file.importer.AjModelImporter;
+import de.tomalbrc.bil.util.ResourcePackUtil;
+import eu.pb4.polymer.resourcepack.api.AssetPaths;
 import net.minecraft.resources.ResourceLocation;
 import org.joml.Vector3f;
+
+import java.nio.charset.StandardCharsets;
 
 public class PlayerModelImporter extends AjModelImporter {
     static Vector3f LIMB_SCALE = new Vector3f(0.46875f,1.40625f,0.46875f);
@@ -16,26 +20,49 @@ public class PlayerModelImporter extends AjModelImporter {
     }
 
     @Override
-    protected ResourceLocation getModelPath(BbOutliner outliner) {
-        switch (outliner.name) {
-            case "head": return generateHeadModel(outliner, new Vector3f(0,5.90f,0), new Vector3f(0.9375f,0.9375f,0.9375f));
-            case "body": return generateHeadModel(outliner, new Vector3f(0,8.25f + 0.25f,0), new Vector3f(0.9375f,1.40625f,0.46875f));
-            case "arm_r": return generateHeadModel(outliner, new Vector3f(0.35f,0.5f,0), LIMB_SCALE);
-            case "arm_l": return generateHeadModel(outliner, new Vector3f(-0.35f,0.5f,0), LIMB_SCALE);
-            case "leg_r": return generateHeadModel(outliner, new Vector3f(0.2f,-1.9f + 0.25f,0), LIMB_SCALE);
-            case "leg_l": return generateHeadModel(outliner, new Vector3f(-0.2f,-1.9f + 0.25f,0), LIMB_SCALE);
+    protected ResourceLocation generateModel(BbOutliner outliner) {
+        return switch (outliner.name) {
+            case "head" -> generateHeadModel(outliner, new Vector3f(0, 5.90f, 0), new Vector3f(0.9375f, 0.9375f, 0.9375f));
+            case "body" -> generateHeadModel(outliner, new Vector3f(0, 8.25f + 0.25f, 0), new Vector3f(0.9375f, 1.40625f, 0.46875f));
+            case "arm_r" -> generateHeadModel(outliner, new Vector3f(0.35f, 0.5f, 0), LIMB_SCALE);
+            case "arm_l" -> generateHeadModel(outliner, new Vector3f(-0.35f, 0.5f, 0), LIMB_SCALE);
+            case "leg_r" -> generateHeadModel(outliner, new Vector3f(0.2f, -1.9f + 0.25f, 0), LIMB_SCALE);
+            case "leg_l" -> generateHeadModel(outliner, new Vector3f(-0.2f, -1.9f + 0.25f, 0), LIMB_SCALE);
             //case "sarm_r": return generateHeadModel(outliner, new Vector3f(-5.125f,0.f,0.f), new Vector3f(0.3515625f,1.40625f,0.46875f)); // todo: slim support
             //case "sarm_l": return generateHeadModel(outliner, new Vector3f(5.125f,0.f,0.f), new Vector3f(0.3515625f,1.40625f,0.46875f)); // pos might be wrong too
-        }
-
-        return super.getModelPath(outliner);
+            default -> super.generateModel(outliner);
+        };
     }
 
     protected ResourceLocation generateHeadModel(BbOutliner outliner, Vector3f pos, Vector3f scale) {
-        ResourcePackItemModel.Builder builder = new ResourcePackItemModel.Builder(model.modelIdentifier)
+        ResourcePackModel.Builder builder = new ResourcePackModel.Builder(model.modelIdentifier)
                 .withParent("builtin/entity")
-                .addDisplayTransform("head", new ResourcePackItemModel.DisplayTransform(null, pos, scale));
+                .addDisplayTransform("head", new ResourcePackModel.DisplayTransform(null, pos, scale));
 
-        return BbResourcePackGenerator.addModelPart(model, outliner.name.toLowerCase(), builder.build());
+        return addItemModel(model, outliner.name.toLowerCase(), builder.build());
+    }
+
+    public static ResourceLocation addItemModel(BbModel model, String partName, ResourcePackModel resourcePackModel) {
+        var modelPath = BbResourcePackGenerator.addModelPart(model, partName, resourcePackModel);
+
+        var str = """
+                {
+                  "model": {
+                    "type": "minecraft:special",
+                    "base": "%s",
+                    "model": {
+                      "type": "minecraft:head",
+                      "kind": "player"
+                    }
+                  }
+                }
+                """;
+
+        var bytes = str.formatted(modelPath.toString()).getBytes(StandardCharsets.UTF_8);
+
+        var id = ResourceLocation.fromNamespaceAndPath("bil", partName);
+        ResourcePackUtil.add(ResourceLocation.parse(":" + AssetPaths.itemAsset(id)), bytes);
+
+        return id;
     }
 }
