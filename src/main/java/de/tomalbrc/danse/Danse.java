64c1fe7c748ed1061c5728f2c70bd189586c9c71
tomalbrc <tomalbrc@me.com>
2025-05-12 13:05:13 +0200

    feat: load models from config dir

--- src/main/java/de/tomalbrc/danse/Danse.java
+++ src/main/java/de/tomalbrc/danse/Danse.java
@@ -1,5 +1,7 @@
 package de.tomalbrc.danse;
 
+import com.mojang.logging.LogUtils;
+import de.tomalbrc.danse.bbmodel.PlayerModelLoader;
 import de.tomalbrc.danse.commands.GestureCommand;
 import de.tomalbrc.danse.registries.EntityRegistry;
 import de.tomalbrc.danse.registries.PlayerModelRegistry;
@@ -8,10 +10,20 @@ import eu.pb4.polymer.resourcepack.api.ResourcePackBuilder;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.Logger;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Danse implements ModInitializer {
     public static final String MODID = "danse";
-
+    public static Logger LOGGER = LogUtils.getLogger();
     public static ResourcePackBuilder RPBUILDER;
 
     @Override
@@ -20,12 +32,47 @@ public class Danse implements ModInitializer {
         PolymerResourcePackUtils.markAsRequired();
 
         ModConfig.load();
-        PlayerModelRegistry.load();
+
+        try {
+            copyResource("danse", "default.ajblueprint", "loosely-coupled.ajblueprint", "tightly-coupled.ajblueprint");
+            for (Path path : loadFiles("danse", ".ajblueprint")) {
+                Danse.LOGGER.info("Loading player gesture model: {}", path.getFileName());
+                PlayerModelRegistry.load(PlayerModelLoader.load(path.toAbsolutePath().toString()));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         EntityRegistry.registerMobs();
 
         PolymerResourcePackUtils.RESOURCE_PACK_CREATION_EVENT.register(resourcePackBuilder -> RPBUILDER = resourcePackBuilder);
         ServerPlayConnectionEvents.JOIN.register((serverGamePacketListener, packetSender, minecraftServer) -> GestureController.onConnect(serverGamePacketListener.player));
         ServerPlayConnectionEvents.DISCONNECT.register((serverGamePacketListener, minecraftServer) -> GestureController.onDisconnect(serverGamePacketListener.player));
         CommandRegistrationCallback.EVENT.register((dispatcher, context, selection) -> GestureCommand.register(dispatcher));
+    }
+
+    private static List<Path> loadFiles(String subDir, String suffix) throws IOException {
+        Path dir = FabricLoader.getInstance().getConfigDir().resolve(subDir);
+        if (!Files.isDirectory(dir)) return List.of();
+        try (Stream<Path> stream = Files.list(dir)) {
+            return stream.filter(p -> p.toString().endsWith(suffix)).toList();
+        }
+    }
+
+    private static void copyResource(String subDir, String... fileNames) throws IOException {
+        Path dir = FabricLoader.getInstance().getConfigDir().resolve(subDir);
+        if (!dir.toFile().exists()) {
+            Files.createDirectories(dir);
+            for (String fileName : fileNames) {
+                Path target = dir.resolve(fileName);
+                if (Files.notExists(target)) {
+                    String resourcePath = "/model/danse/" + fileName;
+                    try (InputStream in = Danse.class.getResourceAsStream(resourcePath)) {
+                        if (in == null) throw new FileNotFoundException("Resource not found: " + fileName);
+                        Files.copy(in, target);
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/de/tomalbrc/danse/registries/PlayerModelRegistry.java
+++ src/main/java/de/tomalbrc/danse/registries/PlayerModelRegistry.java
@@ -1,10 +1,8 @@
 package de.tomalbrc.danse.registries;
 
-import com.google.common.collect.ImmutableList;
 import de.tomalbrc.bil.core.model.Model;
-import de.tomalbrc.danse.bbmodel.PlayerModelLoader;
+import de.tomalbrc.danse.Danse;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-import net.minecraft.resources.ResourceLocation;
 
 import java.util.List;
 import java.util.Map;
@@ -12,19 +10,10 @@ import java.util.Map;
 public class PlayerModelRegistry {
     static Map<String, Model> animationModelMap = new Object2ObjectOpenHashMap<>();
 
-    public static void load() {
-        var sourceModels = ImmutableList.of(
-                "loosely-coupled",
-                "tightly-coupled",
-                "default"
-                // add more if needed
-        );
-
-        for (String filename : sourceModels) {
-            Model model = new PlayerModelLoader().loadResource(ResourceLocation.fromNamespaceAndPath("danse", filename));
-            for (String animationName : model.animations().keySet()) {
-                animationModelMap.put(animationName, model);
-            }
+    public static void load(Model model) {
+        for (String animationName : model.animations().keySet()) {
+            Danse.LOGGER.info("Loaded animation {}", animationName);
+            animationModelMap.put(animationName, model);
         }
     }
 
