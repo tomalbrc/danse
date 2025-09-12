7ab65ad9fb62eb28a16511e410e4656e203734f4
tomalbrc <tomalbrc@me.com>
2025-09-12 16:52:55 +0200

    feat: open dialog through command

--- src/main/java/de/tomalbrc/danse/command/GestureCommand.java
+++ src/main/java/de/tomalbrc/danse/command/GestureCommand.java
@@ -4,11 +4,14 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import de.tomalbrc.bil.util.Permissions;
+import de.tomalbrc.danse.Danse;
 import de.tomalbrc.danse.GestureController;
 import de.tomalbrc.danse.ModConfig;
 import de.tomalbrc.danse.registry.PlayerModelRegistry;
+import de.tomalbrc.danse.util.GestureDialog;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerPlayer;
 
 import static net.minecraft.commands.Commands.literal;
@@ -18,6 +21,13 @@ public class GestureCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         LiteralCommandNode<CommandSourceStack> gestureNode = Commands
                 .literal("gesture").requires(Permissions.require("danse.animation", 1).or((s) -> !ModConfig.getInstance().permissionCheck))
+                .executes(ctx -> {
+                    var player = ctx.getSource().getPlayer();
+                    if (player != null) {
+                        player.openDialog(Holder.direct(GestureDialog.DIALOG));
+                    }
+                    return Command.SINGLE_SUCCESS;
+                })
                 .build();
 
         dispatcher.getRoot().addChild(gestureNode);
