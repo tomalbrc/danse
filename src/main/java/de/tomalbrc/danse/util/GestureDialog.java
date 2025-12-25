45154619c5661573ca248202d24b63484413add6
tomalbrc <tomalbrc@me.com>
2025-12-25 21:25:19 +0100

    fix: dialog layout

--- src/main/java/de/tomalbrc/danse/util/GestureDialog.java
+++ src/main/java/de/tomalbrc/danse/util/GestureDialog.java
@@ -39,7 +39,7 @@ public class GestureDialog {
 
     public static void add(boolean register, ResourcePackBuilder resourcePackBuilder, Runnable onFontCreated) {
         int columns = 3;
-        int width = 240;
+        int width = 240  -8;
 
         List<DialogBody> bodies = new ArrayList<>();
         List<DialogEntry> dialogEntries = new ObjectArrayList<>();
@@ -65,33 +65,35 @@ public class GestureDialog {
         resourcePackBuilder.addData("assets/danse/font/gesture.json", d.getBytes(StandardCharsets.UTF_8));
         onFontCreated.run();
 
-        var iconSpace = ComponentAligner.align(Component.literal(" "), TextUtil.Alignment.CENTER, (width - (33 * columns)) / (columns * 2));
+        // we do some manual padding here
+        var sp = (((double) width /columns) -33)/2.; // 33=fixed width of image in the font def. +1
+        Component iconSpace = ComponentAligner.spacer((int)sp);
+        var spacer = ComponentAligner.spacer(width/columns);
 
-        for (int i = 0; i < dialogEntries.size(); i += columns) {
+        for (int row = 0; row < dialogEntries.size(); row += columns) {
             MutableComponent line1 = Component.empty();
             MutableComponent line2 = Component.empty();
             MutableComponent spaceLine = Component.empty();
 
-            for (int j = i; j < i + columns && j < dialogEntries.size(); j++) {
-                var dialogEntry = dialogEntries.get(j);
-                var iconCombined = wrapCmd(dialogEntry.animation, Component.empty().append(iconSpace).append(TextUtil.parse(dialogEntry.icon)).append(iconSpace));
-                var labels = wrapCmd(dialogEntry.animation, ComponentAligner.align(TextUtil.parse(dialogEntry.text), TextUtil.Alignment.CENTER, width / columns));
+            for (int col = row; col < row + columns && col < dialogEntries.size(); col++) {
+                var dialogEntry = dialogEntries.get(col);
+                var icon = wrapCmd(dialogEntry.animation, Component.empty().append(iconSpace).append(TextUtil.parse(dialogEntry.icon)).append(iconSpace));
+                var labels = wrapCmd(dialogEntry.animation, ComponentAligner.align(ComponentAligner.defaultFont(TextUtil.parse(dialogEntry.text)), TextUtil.Alignment.CENTER, width / columns));
 
-                line1.append(iconCombined);
+                line1.append(icon);
                 line2.append(labels);
-                spaceLine.append(wrapCmd(dialogEntry.animation, ComponentAligner.align(ComponentAligner.spacer(8), TextUtil.Alignment.CENTER, width / columns)));
+                spaceLine.append(wrapCmd(dialogEntry.animation, ComponentAligner.align(ComponentAligner.defaultFont(Component.literal("          ")), TextUtil.Alignment.CENTER, width/columns)));
             }
 
             int c = (dialogEntries.size()) % 3;
-            if (c != 0 && i/columns == columns) {
-                var s = ComponentAligner.spacer(width/columns);
-                line1.append(s);
-                line2.append(s);
-                spaceLine.append(s);
+            if (c != 0 && row/columns == columns) {
+                line1.append(spacer);
+                line2.append(spacer);
+                spaceLine.append(spacer);
             }
 
-            Component parsed = ComponentAligner.defaultFont(Component.empty().append(spaceLine).append("\n").append(line1).append("\n").append(spaceLine).append(spaceLine).append("\n").append(line2));
-            bodies.add(new PlainMessage(parsed, width));
+            Component parsed = ComponentAligner.defaultFont(Component.empty().append("\n").append(line1).append("\n").append(spaceLine).append("\n").append(spaceLine).append("\n").append(line2));
+            bodies.add(new PlainMessage(parsed, width+8));
         }
 
         var data = new CommonDialogData(Component.literal("Gestures"), Optional.empty(), true, true, DialogAction.CLOSE, bodies, List.of());
@@ -113,7 +115,6 @@ public class GestureDialog {
                 .withHoverEvent(new HoverEvent.ShowText(Component.literal(StringUtil.capitalize(anim))));
         return Component.empty().withStyle(style).append(component);
     }
-
 
     private static byte[] getIcon(String iconName) {
         var is = Danse.class.getResourceAsStream("/model/danse/" + iconName + ".png");
